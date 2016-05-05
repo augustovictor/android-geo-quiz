e0e27d789ce9fc052ce2ca8dd243bdcc6a8fc9bf
Victor <victoraweb@gmail.com>
2016-05-05 13:52:27 -0300

    Logging states

--- app/src/main/java/com/example/victoraweb/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/victoraweb/geoquiz/QuizActivity.java
@@ -2,9 +2,11 @@ package com.example.victoraweb.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,7 +14,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionRepository = new Question[] {
         new Question(R.string.question_oceans, true),
@@ -22,17 +25,22 @@ public class QuizActivity extends AppCompatActivity {
         new Question(R.string.question_asia, true)
     };
 
+    private static final String TAG = "QuizActivity";
+
     private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
 
 //        Definitions
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.previous_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setText(mQuestionRepository[mCurrentIndex].getTextResId());
 
@@ -60,6 +68,14 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionRepository.length;
+                updateQuestion();
+            }
+        });
+
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,6 +85,7 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    // Actions
     private void updateQuestion() {
         int question = mQuestionRepository[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -85,5 +102,35 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
