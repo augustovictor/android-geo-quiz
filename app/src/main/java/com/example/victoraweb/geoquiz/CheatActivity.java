a97b0c8ff26ac73ea46965f6060d02d5d2daf379
Victor <victoraweb@gmail.com>
2016-05-10 07:48:14 -0300

    Showing cheat answer

--- app/src/main/java/com/example/victoraweb/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/victoraweb/geoquiz/CheatActivity.java
@@ -4,18 +4,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.victoraweb.geoquiz.answer_is_true";
     private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        // getIntent() returns the intent that started the activity
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
